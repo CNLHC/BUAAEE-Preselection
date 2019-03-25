7f3f4759ea45c30275979be2c60f432e5d763a01
CNLHC <2463765697@qq.com>
2019-03-26 07:12:15 +0800

    联合调试前更新

--- Turn3/FrequencyIP/FrequencyMeter/FrequencyMeter.v
+++ Turn3/FrequencyIP/FrequencyMeter/FrequencyMeter.v
@@ -9,8 +9,11 @@ module FrequencyMeter(
 
 reg [63:0] refCountBuf;
 reg [63:0] sigCountBuf;
-reg FSM;
+reg [1:0]FSM;
 wire  releaxSignal;
+reg refLatchFlag;
+reg sigLatchFlag;
+reg forceClearSig;
 
 ReleaxGenerator URG1 (
     .sysClk(sysClk),
@@ -18,55 +21,85 @@ ReleaxGenerator URG1 (
     .enable (FSM),
     .releaxSignal(releaxSignal));
 
-always @(posedge realTimeTikTok or posedge sysRst or posedge releaxSignal) begin
+always @( posedge realTimeTikTok or posedge sysRst) begin
     if(sysRst) begin
         FSM<=0;
+        forceClearSig<=0;
     end
     else begin
         case(FSM)
-            0: begin
+            0: begin //first 0.5s sample  window
                 FSM<=1;
+                forceClearSig<=0;
             end
-            1: begin
+            1: begin //last 0.5s sample window
+                FSM<=2;
+                forceClearSig<=0;
+            end
+            2: begin // releax window
                 FSM<=0;
+                forceClearSig<=1;
+            end
+            3: begin //Unhandled case
+                FSM<=0; 
             end
         endcase
     end
 end
 
 
-always @ (posedge sysClk or posedge sysRst ) begin
+always @ (posedge sysClk or posedge sysRst) begin
     if(sysRst) begin
         refCounter<=0;
         refCountBuf<=0;
     end
     else begin
         case(FSM)
-            0: refCountBuf<=refCountBuf+1;
-            1:begin
-                refCounter<=refCountBuf;
+            0:begin 
+                refCountBuf<=refCountBuf+1;
+                refLatchFlag<=0;
+            end
+            1: begin
+                refCountBuf<=refCountBuf+1;
+                refLatchFlag<=0;
+            end
+            2:begin
+                if(!refLatchFlag)begin
+                    refCounter<=refCountBuf;
+                    refLatchFlag<=1;
+                end
                 refCountBuf<=0;
             end
         endcase
     end
 end
 
-
-always @ (posedge signal or posedge sysRst ) begin
+always @ (posedge signal or posedge sysRst) begin
     if(sysRst) begin
         sigCounter<=0;
         sigCountBuf<=0;
     end
     else begin
         case(FSM)
-            0:sigCountBuf<=sigCountBuf+1;
-            1:begin
-                sigCounter<=sigCountBuf;
+            0:begin 
+                sigCounter<=0;
+                sigCountBuf<=sigCountBuf+1;
+                sigLatchFlag<=0;
+            end
+            1: begin
+                sigCounter<=0;
+                sigCountBuf<=sigCountBuf+1;
+                sigLatchFlag<=0;
+            end
+            2:begin
+                if(!sigLatchFlag)begin
+                    sigCounter<=sigCountBuf;
+                    sigLatchFlag<=1;
+                end
                 sigCountBuf<=0;
             end
         endcase
     end
 end
-
 endmodule
 
--- Turn3/FrequencyIP/FrequencyMeter/PhaseDetector.v
+++ Turn3/FrequencyIP/FrequencyMeter/PhaseDetector.v
@@ -4,15 +4,20 @@ module PhaseDetector(
     input sysRst,
     input CHA,
     input CHB,
-    output [63:0]diffCounter
+    output reg [63:0]diffCounter
 );
-reg [1:0]FSM;
+wire [1:0]FSM;
 reg [63:0] phraseDiffCountBuf;
+reg TickA;
+reg TickB;
+assign FSM={TickA,TickB};
 
 wire  releaxSignal;
 wire  releaxEnable;
 
 assign  releaxEnable=(FSM==2);
+
+
 
 ReleaxGenerator URG1 (
     .sysClk(sysClk),
@@ -27,36 +32,55 @@ always @(posedge workClk or posedge sysRst) begin
     end
     else begin
         case (FSM) 
-            0: begin
+            2'b01: begin
                 phraseDiffCountBuf<=0;
             end
-            1: begin
+            2'b10: begin
                 phraseDiffCountBuf<=phraseDiffCountBuf+1;
             end
-            2: begin
+            2'b11: begin
                 diffCounter<=phraseDiffCountBuf;
+            end
+            2'b00:begin
+                phraseDiffCountBuf<=0;
             end
         endcase
     end
 end
 
-always @(posedge CHA or posedge CHB or posedge sysRst or posedge releaxSignal) begin
+always @(posedge CHA or posedge sysRst ) begin
     if(sysRst) begin
-        FSM=0;
+        TickA<=0;
     end
     else begin
         case (FSM) 
-            0:begin
-                if(CHA)
-                    FSM=1;
+            2'b00:begin
+                TickA<=1;
             end
-            1:FSM=2;
-            2:begin
-                if(releaxSignal)
-                    FSM=0;
+            2'b11:begin
+                TickA<=0;
             end
+            default: TickA<=TickA;
         endcase
     end
 end
+
+always @(posedge CHB or posedge sysRst ) begin
+    if(sysRst) begin
+        TickB<=0;
+    end
+    else begin
+        case (FSM) 
+            2'b10:begin
+                TickB<=1;
+            end
+            2'b01:begin
+                TickB<=0;
+            end
+            default:TickB<=TickB;
+        endcase
+    end
+end
+
 endmodule
 
