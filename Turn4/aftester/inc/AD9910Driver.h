0666460c0e8dae5e910d5a9048b7c7307b5f49fa
CNLHC <2463765697@qq.com>
2019-05-13 03:09:21 +0800

    完成AD9910驱动

--- Turn4/aftester/inc/AD9910Driver.h
+++ Turn4/aftester/inc/AD9910Driver.h
@@ -5,22 +5,26 @@
 #ifndef AFTESTER_SRC_AD9910DRIVER_H_
 #define AFTESTER_SRC_AD9910DRIVER_H_
 #include "libMPSSE_spi.h"
+#include "FTD2xxBitMode.h"
 
 class AD9910Driver {
 
 public:
-  AD9910Driver(FT_HANDLE SPIHandle, FT_HANDLE GPIOHandle);
+  AD9910Driver(FT_HANDLE SPIHandle, FT_HANDLE GPIOHandle,uint8 UpdatePinNum,uint32 MPSSEConfig);
   bool WriteRegister(uint8 address, uint8 length, uint8 *data);
   bool ReadRegister(uint8 address, uint8 length, uint8 *buf);
+  void Init();
+  void setSingleTuneOutput(uint16 amp,double Freq);
 
 private:
   void pIOUSet();
   void pIOUReset();
-
   FT_HANDLE mSPIHandle;
   FT_HANDLE mGPIOHandle;
-  uint8 mIOUpdateSet;
-  uint8 mIOUpdateReSet;
+  uint8  mIOUpdatePinNo;
+  uint32 mMPSSEConfig;
+  FTD2xxBitMode mBitMode;
+
 };
 
 #endif // AFTESTER_SRC_AD9910DRIVER_H_
